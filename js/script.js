05e3ab7a8b7c631f65f6aa9e5231751865943936
Yu Nandar <abc@gmail.com>
2022-09-12 10:24:03 +0630

    callclean

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// const header = document.querySelector("header");
+
+// const signupBtn = document.querySelector(".signupBtn");
+// const loginBtn = document.querySelector(".loginBtn");
+// const moveBtn = document.querySelector(".moveBtn");
+// const signup = document.querySelector(".signup");
+// const login = document.querySelector(".login");
+
+
+// window.addEventListener("scroll", function(){
+//     header.classList.toggle("sticky", this.window.scrollY > 0);
+// });
+
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// };
+
+// window.onscroll = () =>{
+//     menu.classList.remove('bx-x');
+//     navbar.classList.remove('active');
+// };
+
+// loginBtn.addEventListener("click", () => {
+//     moveBtn.classList.add("rightBtn");
+//     login.classList.add("loginForm");
+//     signup.classList.remove("signupForm");
+//     moveBtn.innerHTML = "Login";
+// });
+
+// signupBtn.addEventListener("click", () => {
+//     moveBtn.classList.remove("rightBtn");
+//     login.classList.remove("loginForm");
+//     signup.classList.add("signupForm");
+//     moveBtn.innerHTML = "Signup";
+// });
+
+// document.querySelector("#prev").onclick = prev;
+// document.querySelector("#next").onclick = next;
+
+setInterval(next, 2000);
+
+let pic = 1;
+
+function prev() {
+    document.querySelector("#pic" + pic).style.display = "none";
+    pic--;
+    if(pic < 1) pic = 6;
+    document.querySelector("#pic" + pic).style.display = "block";
+}
+
+function next() {
+    document.querySelector("#pic" + pic).style.display = "none";
+    pic++;
+    if(pic > 6) pic = 1;
+    document.querySelector("#pic" + pic).style.display = "block";
+}
+
+
