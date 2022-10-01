3c872db3f64ef3ec5e9789a566e90a3869160bbd
Yu Nandar <abc@gmail.com>
2022-10-01 12:09:18 +0630

    responsive for table and mobile

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 const header = document.querySelector("header");
-
-// const signupBtn = document.querySelector(".signupBtn");
-// const loginBtn = document.querySelector(".loginBtn");
-// const moveBtn = document.querySelector(".moveBtn");
-// const signup = document.querySelector(".signup");
-// const login = document.querySelector(".login");
-
-
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0);
 });
@@ -19,24 +11,7 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
+    menu.classList.remove('active');
 };
-
-// loginBtn.addEventListener("click", () => {
-//     moveBtn.classList.add("rightBtn");
-//     login.classList.add("loginForm");
-//     signup.classList.remove("signupForm");
-//     moveBtn.innerHTML = "Login";
-// });
-
-// signupBtn.addEventListener("click", () => {
-//     moveBtn.classList.remove("rightBtn");
-//     login.classList.remove("loginForm");
-//     signup.classList.add("signupForm");
-//     moveBtn.innerHTML = "Signup";
-// });
-
-// 
-
